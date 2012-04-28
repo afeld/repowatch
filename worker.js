86ad05bf9b40770f62eb0f0e035dda210bab7493
Aidan Feldman <aidan.feldman@gmail.com>
2012-04-28 10:30:12 -0400

    refactor out findMostRecent()

--- worker.js
+++ worker.js
@@ -14,6 +14,21 @@ function getRepoList(){
   ];
 }
 
+function findMostRecent(repo, callback){
+  var execOpts = { cwd: 'tmp/' + repo.name };
+  // get most recent tag
+  exec('git describe --abbrev=0 --tags', execOpts, function(error, stdout, stderr){
+    // console.log(error, stdout, stderr);
+    var tag = stdout.trim();
+    if (tag){
+      callback(tag);
+    } else {
+      // TODO
+      callback();
+    }
+  });
+}
+
 var repos = getRepoList();
 repos.forEach(function(repo){
   var ghRepo = repo.user + '/' + repo.name,
@@ -22,14 +37,11 @@ repos.forEach(function(repo){
   fs.mkdir('tmp', function(){
     // clone and/or update repo
     exec('cd tmp && git clone ' + url + ' && cd ' + repo.name + '.git && git pull', function(){
-      var execOpts = { cwd: 'tmp/' + repo.name };
-      // get most recent tag
-      exec('git describe --abbrev=0 --tags', execOpts, function(error, stdout, stderr){
-        var tag = stdout.trim();
-        if (tag){
-          console.log(ghRepo, tag);  
+      findMostRecent(repo, function(version){
+        if (version){
+          console.log(ghRepo, version);
         } else {
-          console.log('tag not found for ' + ghRepo);
+          console.log('version not found for ' + ghRepo);
         }
       });
     });
