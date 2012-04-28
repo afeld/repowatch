efdfd31c9fc388f55d39fc5649a538e963fe7fe2
Aidan Feldman <aidan.feldman@gmail.com>
2012-04-28 12:54:57 -0400

    connect to mongo within app

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-var express = require('express');
+var express = require('express'),
+  mongodb = require('mongodb');
 
 var app = express.createServer();
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
-app.listen(3000);
-console.log('app started on port 3000');
+
+var dbClient = new mongodb.Db('repowatch', new mongodb.Server('ds033107.mongolab.com', 33107, {}));
+dbClient.open(function(error){
+  if (error){
+    console.error(error);
+  } else {
+    var dbPass = process.env.REPOWATCH_DB_PASS;
+    if (!dbPass) throw "REPOWATCH_DB_PASS password not set";
+    dbClient.authenticate('admin', dbPass, function(){
+      app.listen(3000);
+      console.log('app started on port 3000');
+    });
+  }
+});
+
