fe493ff4c3e50a5049716d4ac2c9c2b34fd59c3d
Aidan Feldman <aidan.feldman@gmail.com>
2012-04-30 23:42:51 -0400

    add form to submit email address and repo URL

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 var express = require('express'),
   mongodb = require('mongodb');
 
+var dbClient;
+
 var app = express.createServer();
+app.use(express.logger());
+app.use(express.bodyParser());
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  res.render('index', {notice: null});
+});
+
+app.post('/submit', function(req, res){
+  var usersColl = new mongodb.Collection(dbClient, 'users'),
+    userInfo = req.body.user,
+    email = userInfo.email;
+
+  console.log(req.body);
+  var doc = {
+    '$set': {email: email},
+    '$addToSet': {
+      repos: {
+        url: userInfo.repo
+      }
+    }
+  };
+
+  usersColl.update({email: email}, doc, {safe: true, upsert: true}, function(err){
+    var notice = err || 'Submitted!';
+    res.render('index', {notice: notice});
+  });
 });
 
 
-var dbClient = new mongodb.Db('repowatch', new mongodb.Server('ds033107.mongolab.com', 33107, {}));
+dbClient = new mongodb.Db('repowatch', new mongodb.Server('ds033107.mongolab.com', 33107, {}));
 dbClient.open(function(error){
   if (error){
     console.error(error);
