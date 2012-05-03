cde93347527bdf2d4dd37b24114abdaaedf88d6a
Aidan Feldman <aidan.feldman@gmail.com>
2012-05-03 03:58:00 -0400

    add unsubscribe button on settings page

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var mongoose = require('mongoose'),
   User = require('./models/user').model;
 
 
+app.dynamicHelpers({
+  flash: function(req, res){
+    return {
+      error: req.flash('error') || [],
+      info: req.flash('info') || []
+    };
+  }
+});
+
+
 app.get('/', function(req, res){
   if (req.user) return res.redirect('/settings'); // already signed in
   res.render('index');
@@ -12,6 +22,20 @@ app.get('/settings', function(req, res){
   var user = req.user;
   if (!user) return res.redirect('/'); // needs to be signed in
   res.render('settings', { user: user });
+});
+
+app.post('/unsubscribe', function(req, res){
+  var user = req.user;
+  if (!user) return res.redirect('/'); // needs to be signed in
+  user.remove(function(err){
+    if (err){
+      req.flash('error', err);
+    } else {
+      req.flash('info', 'You have been removed from the system');
+    }
+    
+    res.redirect('/logout');
+  });
 });
 
 
