3512c57f94825a331e0780133437e2bdb31d1bba
Aidan Feldman <aidan.feldman@gmail.com>
2012-05-12 20:18:43 -0400

    fixed worker to use new Repo collection
    loops through repos and updates them, then notifies matching users

--- worker.js
+++ worker.js
@@ -1,19 +1,32 @@
-var User = require('./models/user');
+var Repo = require('./models/repo'),
+  User = require('./models/user');
 
 
-User.find().each(function(err, user){
-  if (!user) return; // sometimes it's null..?
-  console.log('user', user);
+// update all repo versions
+Repo.find().each(function(err, repo){
+  if (!repo) return; // complete
 
-  user.repos.forEach(function(repo){
-    repo.findLatestVersion(function(err, version){
-      var ghRepo = repo.ghRepo;
-      if (version){
-        console.log(ghRepo, version);
-        user.notifyNewVersion(repo, version);
-      } else {
-        console.log('version not found for ' + ghRepo);
+  repo.findLatestVersion(function(err, newVersion){
+    var ghRepo = repo.ghRepo,
+      oldVersion = repo.version;
+
+    if (newVersion){
+      console.log(ghRepo, oldVersion, newVersion);
+      if (newVersion !== oldVersion){
+        // TODO update stored version number
+
+        // notify users watching this repo of new version
+        User.find({ repo_ids: repo._id }).each(function(err, user){
+          if (!user) return; // complete
+
+          console.log('notifying user ' + user.github.login + ' about ' + ghRepo + ' ' + newVersion);
+          user.notifyNewVersion(repo, newVersion);
+        });
       }
-    });
+    } else {
+      console.log('version not found for ' + ghRepo);
+    }
   });
 });
+
+// TODO exit when all are completed
